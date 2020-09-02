53aac6ef601c012d32ce8b2a8c1428f9c315f829
gabri <gabri@DESKTOP-FOSCRT8>
2020-09-02 00:15:22 -0300

    criação de entidades 0.1

---
+++ QuickBuy.Dominio/Entidades/ItemPedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class ItemPedido
+    {
+        public int Id { get; set; }
+        public string ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public string NumeroEndereco { get; set; }
+
+
+
+
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,22 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public ICollection<ItemPedido> itensPedido { get; set; }
+
+
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+
+
+    }
+}
